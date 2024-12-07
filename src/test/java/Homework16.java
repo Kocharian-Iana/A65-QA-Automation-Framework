ea0eef294f33f0a02ad158b3c4530cceb2e382c1
Kocharian-Iana <145826276+Kocharian-Iana@users.noreply.github.com>
2024-12-06 23:25:22 -0600

    added login tests and fixed HW_17

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -1,18 +1,14 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework16 extends BaseTest {
     @Test
-    public void registrationNavigation(){
+    public void registrationNavigation() {
         WebElement registrationButton = driver.findElement(By.cssSelector("a[href='registration']"));
         registrationButton.click();
         WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-
         Assert.assertTrue(emailField.isDisplayed());
     }
 }
--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,48 +1,42 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 public class Homework17 extends BaseTest {
-   @Test
+    @Test
     public void addSongToPlaylist() throws InterruptedException {
-       WebElement loginField = driver.findElement(By.cssSelector("[type='email']"));
-       WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-       WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+        WebElement loginField = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
 
-
-       loginField.sendKeys("iana.kocharian@testpro.io");
-       passwordField.sendKeys("CwqOPgQw");
-       submitButton.click();
-       WebElement searchField = driver.findElement(By.cssSelector("[type='search']"));
-       searchField.sendKeys("BossStatus");
-       searchField.sendKeys(Keys.ENTER);
-       WebElement viewAllButton = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
-       viewAllButton.click();
-
-       WebElement highlightSongInTheList = driver.findElement(By.xpath("/html//section[@id='songResultsWrapper']//table[@class='items']//td[@class='title']"));
-       highlightSongInTheList.click();
-       WebElement addToButton = driver.findElement(By.cssSelector(".btn-add-to"));
-       addToButton.click();
-       WebElement createNewPlaylist = driver.findElement(By.cssSelector("#songResultsWrapper [required]"));
-       createNewPlaylist.sendKeys("test1");
-       createNewPlaylist.sendKeys(Keys.ENTER);
-       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-       WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
-       String testMessage = successMessage.getText();
-       Assert.assertTrue(successMessage.isDisplayed());
-       Assert.assertTrue(testMessage.contains("Created playlist \"test1.\""));
-      WebElement deletePlaylistButton = driver.findElement(By.cssSelector(".btn-delete-playlist"));
-      deletePlaylistButton.click();
-      WebElement okButton = driver.findElement(By.cssSelector(".ok"));
-      okButton.click();
+        loginField.sendKeys("iana.kocharian@testpro.io");
+        passwordField.sendKeys("CwqOPgQw");
+        submitButton.click();
+        WebElement searchField = driver.findElement(By.cssSelector("[type='search']"));
+        searchField.sendKeys("BossStatus");
+        searchField.sendKeys(Keys.ENTER);
+        WebElement viewAllButton = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
+        viewAllButton.click();
+        WebElement highlightSongInTheList = driver.findElement(By.xpath("/html//section[@id='songResultsWrapper']//table[@class='items']//td[@class='title']"));
+        highlightSongInTheList.click();
+        WebElement addToButton = driver.findElement(By.cssSelector(".btn-add-to"));
+        addToButton.click();
+        WebElement createNewPlaylist = driver.findElement(By.cssSelector("#songResultsWrapper [required]"));
+        createNewPlaylist.sendKeys("test1");
+        createNewPlaylist.sendKeys(Keys.ENTER);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        String testMessage = successMessage.getText();
+        Assert.assertTrue(successMessage.isDisplayed());
+        Assert.assertTrue(testMessage.contains("Created playlist \"test1.\""));
+        WebElement deletePlaylistButton = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+        deletePlaylistButton.click();
+        WebElement okButton = driver.findElement(By.cssSelector(".ok"));
+        okButton.click();
     }
 
 }
