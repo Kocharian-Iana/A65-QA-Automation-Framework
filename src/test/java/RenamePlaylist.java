68721dd0b89630c7c5ae76bc196df7ccacc26543
Kocharian-Iana <145826276+Kocharian-Iana@users.noreply.github.com>
2024-12-19 14:25:28 -0600

    added loop to RenamePlaylist.java and AfterMethod deletePlayList

--- src/test/java/RenamePlaylist.java
+++ src/test/java/RenamePlaylist.java
@@ -3,26 +3,43 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class RenamePlaylist extends BaseTest {
+    String currentPlayListName = "Test";
+    String newPlayListName = "Succeed";
+
     @Test
     public void renamePlayListByDoubleClick() {
+
         login("iana.kocharian@testpro.io", "CwqOPgQw");
         WebElement plusButton = driver.findElement(By.cssSelector(".fa.fa-plus-circle.create"));
         plusButton.click();
-        WebElement NewPlayListButton = driver.findElement(By.cssSelector("[data-testid='playlist-context-menu-create-simple']"));
-        NewPlayListButton.click();
-        WebElement PlayListNameField = driver.findElement(By.cssSelector("input[placeholder='↵ to save']"));
-        PlayListNameField.sendKeys("Test");
-        PlayListNameField.sendKeys(Keys.ENTER);
-        WebElement playList = driver.findElement(By.xpath("//a[contains(text(), 'Test')]"));
+        WebElement newPlayListButton = driver.findElement(By.cssSelector("[data-testid='playlist-context-menu-create-simple']"));
+        newPlayListButton.click();
+        WebElement playListNameField = driver.findElement(By.cssSelector("input[placeholder='↵ to save']"));
+        playListNameField.sendKeys(currentPlayListName);
+        playListNameField.sendKeys(Keys.ENTER);
+        WebElement playList = driver.findElement(By.xpath(String.format("//a[contains(text(), '%s')]", currentPlayListName)));
+
         actions.doubleClick(playList).perform();
-        WebElement playListEditing = driver.findElement(By.cssSelector("li.editing input[data-testid='inline-playlist-name-input']"));
-        playListEditing.sendKeys("Succeed");
-        playListEditing.sendKeys(Keys.ENTER);
+        WebElement playListInput = driver.findElement(By.cssSelector("[data-testid = 'inline-playlist-name-input']"));
+        for (int i = 0; i < currentPlayListName.length(); i++) {
+            playListInput.sendKeys(Keys.BACK_SPACE);
+        }
+        playListInput.sendKeys(newPlayListName);
+        playListInput.sendKeys(Keys.ENTER);
         wait.until(ExpectedConditions.visibilityOf(playList));
-        Assert.assertEquals(playList.getText(), "TestSucceed");
-        //нужно удалить вконце плейлист!! доделать!!
+        Assert.assertEquals(playList.getText(), newPlayListName);
+
+    }
+
+    @AfterMethod
+    public void deletePlayList() {
+        WebElement playList = driver.findElement(By.xpath(String.format("//a[contains(text(), '%s')]", newPlayListName)));
+        actions.contextClick(playList).perform();
+        WebElement deleteButton = driver.findElement(By.xpath("//li [contains(text(), 'Delete')]"));
+        deleteButton.click();
     }
 }
